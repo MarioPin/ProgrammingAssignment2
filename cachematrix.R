0fae269fce119ec5c1f37e6506cb6ca367670e17
MarioPin <mariopintado@yahoo.com>
2014-05-23 14:20:42 -0400

    Assignment 2
    Started with the sample Mean functions given in the assignment and
    modified to get the inverse.

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,21 @@
-## Put comments here that give an overall description of what your
+## Write Function that is given a matrix and can cache its inverse
 ## functions do
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL                       
+        set <- function(y) {            
+                x <<- y                 
+                m <<- NULL              
+        }
+        get <- function() x
+        setInverse <- function(inverse) m <<- inverse
+        getInverse <- function() m
+        list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
+        
 
 }
 
@@ -12,4 +24,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        InvM <- x$getInverse()
+        if(!is.null(InvM)) {
+                message("getting cached data")
+                return(InvM)
+        }
+        data <- x$get()
+        InvM <- solve(data)
+        x$setInverse(InvM)
+        InvM
 }
